9223c3d4d01676c98aa469bfd6804f2209b176c1
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-10 14:35:48 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+    public static void main(String[] args) {
+        //variables
+        int a= 12;
+        int b= 35;
+        int sum = a+b;
+        System.out.println(sum);
+        int mul = a*b;
+        System.out.println(mul);
+        int sub = a-b;
+        System.out.println(sub);
+
+        String name = "Sai Khairnar";
+        int age = 21;
+        double marks = 83.93;
+        System.out.println(age);
+    }
+
+}
