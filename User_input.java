1cece40c7f81cc1d415321709f2810ade3343088
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-10 14:48:04 +0530

    Add files via upload

---
+++ User_input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class User_input {
+    public static void main(String[] args) {
+     //Input
+        Scanner sc = new Scanner(System.in);
+        int Age = sc.nextInt();
+        System.out.println(Age);
+      }
+}
