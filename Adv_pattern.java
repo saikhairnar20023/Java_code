08993c8eb57c31fc35c49eb35a28bf18872ef7c4
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-17 18:42:27 +0530

    Add files via upload

---
+++ Adv_pattern.java
@@ -0,0 +1,118 @@
+public class Adv_pattern {
+    public static void main (String[] args){
+        // Advance pattern
+
+//        for (int i = 1;i<=5;i++){
+//            for (int j = 1;j<5;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println("*");
+//
+//        }
+//          int n = 4;//rows
+//          int m = 5;//col
+//          //outer loop
+//          for (int i = 1;i<=n;i++){
+//              //inner loop
+//              for (int j =1; j<=m;i++){
+//                  if (i==1 ||  j==5 || i==n || j==m){
+//                     System.out.print("*");
+//                  } else {
+//                      System.out.print(" ");
+//                  }
+//              }
+//              System.out.println("*");
+//
+//          }
+
+             //another pattern
+
+//              int n= 10;//rows
+//              //inner loop
+//              for (int i = 1; i<=n;i++){
+//                  //outer loop
+//                  for (int j=1;j<=i;j++){
+//                      System.out.print("*");
+//                  }
+//                  System.out.println();
+//              }
+
+              //another one
+//              int n= 6;//row
+//              //outer loop
+//              for (int i = n; i>=1;i--){
+//                  //inner
+//                  for (int j=1;j<=i;j++){
+//                      System.out.print("*");
+//                  }
+//                  System.out.println();
+//              }
+
+                //another example
+//        int n= 4;
+//        for (int i= 1;i<=n;i++){
+//            for(int j=1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//          int n=5;
+//          for(int i =1;i<=n;i++){
+//              for(int j=1;j<=i;j++){
+//                  System.out.print(j+" ");
+//              }
+//              System.out.println();
+//          }
+
+//        int n= 5;
+//        for (int i= 1;i<=n;i++){
+//            for(int j=1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            for (int j=1;j<=i;j++){
+//                System.out.print(j+" " );
+//            }
+//            System.out.println();
+//       }
+
+
+//        int n= 5;
+//        for (int i = 1;i<=n;i++){
+//            for (int j=1;j<=n-i+1;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+        //floyd's triangle
+//         int n= 5;
+//         int num= 1;
+//         for(int i=1;i<=n;i++){
+//             for (int j = 1;j<=i;j++){
+//                 System.out.print(num);
+//                 num++;
+//             }
+//             System.out.println();
+//         }
+
+        //0-1 triangle
+          int n= 5;
+          for (int i =1;i<=n;i++){
+              for (int j=1;j<=i;j++){
+                  int sum = i+j ;
+                  if ((sum %2==0)){
+                      System.out.print("1 ");
+                  }else {
+                      System.out.print("0 ");
+                  }
+              }
+              System.out.println();
+          }
+
+
+    }
+}
