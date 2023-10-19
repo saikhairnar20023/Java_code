905e8ce9f6126441be1cc12ab4f536f1fd4adf68
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-19 12:19:54 +0530

    Add files via upload

---
+++ Functions_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Functions_ {
+    public static int calculatemul(int a,int b){
+        return a*b;
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Product of a and b is" + calculatemul(a,b));
+    }
+}
