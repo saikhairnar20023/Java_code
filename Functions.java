03c9851740bc3fa49f1331817781653d342cccd5
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-19 12:18:47 +0530

    Add files via upload

--- Functions.java
+++ Functions.java
@@ -1,17 +1,30 @@
 import java.util.*;
-public class Functions {
+//public class Functions {
 //    public static void printmyname(String name) {
 //        System.out.println(name);
 //        return;
 //
 //    }
- public static void main(String[] args) {
+// public static void main(String[] args) {
 //      Scanner sc = new Scanner(System.in);
 //      String name = sc.next();
 //
 //      printmyname(name);
 
-       
 
+//     public class Functions_ {
+//         public static int calculatesum(int a,int b){
+//             int sum =  a +  b;
+//             return sum;
+//
+//         }
+//         public static void main(String[] args){
+//             Scanner sc = new Scanner(System.in);
+//             int a = sc.nextInt();
+//             int b = sc.nextInt();
+//             int sum = calculatesum(a,b);
+//             System.out.println("Sum of a and b is" + sum);
+//         }
+     }
     }
 }
