7dda03df7eb14aef21ef7cb0e90ed75519f47f11
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-19 12:02:11 +0530

    Add files via upload

--- Adv_pattern.java
+++ Adv_pattern.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Adv_pattern {
     public static void main (String[] args){
         // Advance pattern
@@ -100,18 +102,33 @@ public class Adv_pattern {
 //         }
 
         //0-1 triangle
-          int n= 5;
-          for (int i =1;i<=n;i++){
-              for (int j=1;j<=i;j++){
-                  int sum = i+j ;
-                  if ((sum %2==0)){
-                      System.out.print("1 ");
-                  }else {
-                      System.out.print("0 ");
-                  }
-              }
-              System.out.println();
-          }
+//          int n= 5;
+//          for (int i =1;i<=n;i++){
+//              for (int j=1;j<=i;j++){
+//                  int sum = i+j ;
+//                  if ((sum %2==0)){
+//                      System.out.print("1 ");
+//                  }else {
+//                      System.out.print("0 ");
+//                  }
+//              }
+//              System.out.println();
+//          }
+        Scanner sc= new Scanner(System.in);
+//        int n = 4;
+//        int m= 5;
+//        for(int i =1;i<=n;i++){
+//            for (int j=1;j<=m;j++){
+//                if (i==1 || j==1 || i==n || j==m){
+//                    System.out.print("*");
+//                }else {
+//                    System.out.print(" ");
+//                }
+//
+//            }System.out.println();
+//        }
+
+
 
 
     }
