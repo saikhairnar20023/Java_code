6a1dc10c402ac15a26a226b0eea8ebade2f036d0
Sai Khairnar <85782407+saikhairnar20023@users.noreply.github.com>
2023-10-19 12:27:15 +0530

    Add files via upload

--- Functions_.java
+++ Functions_.java
@@ -1,13 +1,28 @@
 import java.util.*;
 public class Functions_ {
-    public static int calculatemul(int a,int b){
-        return a*b;
+//    public static int calculatemul(int a,int b){
+//        return a*b;
+//
+//    }
+    public static void printfactorial(int a){
+        if (a<=0){
+            System.out.println("invalid number");
+            return;
+        }
+
+        int factorial =1;
+        for(int i=a;i>=1;i--){
+            factorial=factorial *i;
+        }
+        System.out.println(factorial);
 
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("Product of a and b is" + calculatemul(a,b));
+
+        printfactorial(a);
     }
+
+
 }
